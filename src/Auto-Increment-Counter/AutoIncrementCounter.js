f0c1ac4ac44b1515a8e9d1f45486d48b6b991ba6
as088440 <akarsh.srivastava@cerner.com>
2023-08-04 17:10:15 +0530

    akarsh autoincrement

--- src/Auto-Increment-Counter/AutoIncrementCounter.js
+++ src/Auto-Increment-Counter/AutoIncrementCounter.js
@@ -1,35 +1,34 @@
-import "./AutoIncrementCounter.css"
-import { useEffect, useState, useRef } from "react"
+import "./AutoIncrementCounter.css";
+import { useEffect, useState, useRef } from "react";
 
 export default function AutoIncrementCounter() {
-    const [counter, setCounter] =  useState(0);
-    const [pause, setPause] =  useState(false);
-    let intervalTimer  = useRef(null)
+    const [counter, setCounter] = useState(0);
+    const [pause, setPause] = useState(false);
+    const intervalTimer = useRef(null);
 
-    const startTimer = () => {
-        intervalTimer.current = setInterval(()=> {
-            setCounter((prevCounter) => prevCounter + 1)
-        }, 1000)
-        setPause(true)
-    }
-
-    const pauseTimer = () => {
-        clearInterval(intervalTimer.current);
-        setPause(false)
-    }
+    const toggleTimer = () => {
+        if (pause) {
+            clearInterval(intervalTimer.current);
+        } else {
+            intervalTimer.current = setInterval(() => {
+                setCounter((prevCounter) => prevCounter + 1);
+            }, 1000);
+        }
+        setPause((prevPause) => !prevPause);
+    };
 
     const resetTimer = () => {
-        setPause(false);
         clearInterval(intervalTimer.current);
         setCounter(0);
-    }
+        setPause(false);
+    };
 
     return (
         <div className="App">
             <h1>Counter</h1>
             <div>{counter}</div>
-            <button onClick={pause ? pauseTimer: startTimer}>{pause ? "Pause": "Start"}</button>
+            <button onClick={toggleTimer}>{pause ? "Pause" : "Start"}</button>
             <button onClick={resetTimer}>Reset</button>
         </div>
-    )
+    );
 }
