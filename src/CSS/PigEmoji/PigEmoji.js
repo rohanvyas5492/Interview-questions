18b1a8a4aa2b91a4d025b342c446ab0c549eed9b
Akarsh <akarsh1888@gmail.com>
2023-08-09 16:53:48 +0000

    Update index.js and yarn.lock

---
+++ src/CSS/PigEmoji/PigEmoji.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./PigEmoji.css";
+
+export default function PigEmoji() {
+  return (
+    <div class="head">
+      <div>
+        <div class="ear"></div>
+        <div class="ear"></div>
+      </div>
+
+      <div>
+        <div class="eye">
+          <div class="pupil"></div>
+        </div>
+        <div class="eye">
+          <div class="pupil"></div>
+        </div>
+      </div>
+
+      <div class="nose">
+        <div class="nostril"></div>
+        <div class="nostril"></div>
+      </div>
+    </div>
+  );
+}
