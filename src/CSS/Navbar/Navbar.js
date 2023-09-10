4c5ee4e980362bace6aef61f0b2d1f3cd6d0dfe4
Akarsh <akarsh1888@gmail.com>
2023-09-10 13:31:57 +0000

    Update 3 files

---
+++ src/CSS/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+export default function Navbar() {
+
+  return (
+    <>
+    <nav>
+    <ul>
+      <li>Products</li>
+      <li>Testimonials</li>
+      <li>Pricing</li>
+      <li>Team</li>
+      <li>Contact Us</li>
+    </ul>
+    </nav>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
   import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import SignUpForm from "./CSS/SignUpForm/SignUpForm";
+import Navbar from "./CSS/Navbar/Navbar";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <SignUpForm />
+    <Navbar />
   </StrictMode>
 );
