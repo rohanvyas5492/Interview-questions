ca9cc7e0515b2c1956b6a4d1afd67ad746f204c7
akarsh1888 <akarsh1888@gmail.com>
2023-09-26 18:56:07 +0530

    StarRating (React Component)

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import InfiniteScroll from "./InfiniteScroll/InfiniteScroll";
+import StarRating from "./StarRating/StarRating";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   const root = createRoot(rootElement);
-  root.render(<InfiniteScroll/>);
+  root.render(<StarRating total={5} value={2}/>);
 } else {
   console.error("Element with id 'root' not found in the DOM.");
 }
